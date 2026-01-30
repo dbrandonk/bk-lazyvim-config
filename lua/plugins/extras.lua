7a65982e91e119b34435252ded826ee099e640af
Brandon Knape <d.brandonk@gmail.com>
2026-01-29 21:15:38 -0600

    Added coding.mini-surround plugin.

--- lua/plugins/extras.lua
+++ lua/plugins/extras.lua
@@ -1,4 +1,5 @@
 return {
   { import = "lazyvim.plugins.extras.coding.yanky" },
   { import = "lazyvim.plugins.extras.editor.mini-files" },
+  { import = "lazyvim.plugins.extras.coding.mini-surround" },
 }
